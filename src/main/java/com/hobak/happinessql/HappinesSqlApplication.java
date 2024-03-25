29490a0b614b26f9bb5cff72cb75b6d7d3e141fc
yel-m <coin6442@gmail.com>
2024-03-25 13:34:59 +0900

    initial commit

---
+++ src/main/java/com/hobak/happinessql/HappinesSqlApplication.java
@@ -0,0 +1,13 @@
+package com.hobak.happinessql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappinesSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappinesSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hobak/happinessql/HappinesSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobak.happinessql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappinesSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
