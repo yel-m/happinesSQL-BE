8e8b57232595b4a183288b492b12552dbaf39db4
SangEun <olsang1203@gmail.com>
2024-03-27 19:22:14 +0900

    fix : User import 에러 수정

--- src/main/java/com/hobak/happinessql/domain/user/repository/UserRespository.java
+++ src/main/java/com/hobak/happinessql/domain/user/repository/UserRespository.java
@@ -1,7 +1,8 @@
 package com.hobak.happinessql.domain.user.repository;
 
+import com.hobak.happinessql.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
+
 
 public interface UserRespository extends JpaRepository<User, Long> {
 }
