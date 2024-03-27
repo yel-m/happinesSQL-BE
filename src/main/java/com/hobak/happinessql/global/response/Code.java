45cdf75c96921ca286427bf2299f86f22e43d684
yel-m <coin6442@gmail.com>
2024-03-27 18:41:01 +0900

    feat : API 응답 포맷 통일

---
+++ src/main/java/com/hobak/happinessql/global/response/Code.java
@@ -0,0 +1,72 @@
+package com.hobak.happinessql.global.response;
+
+import com.hobak.happinessql.global.exception.GeneralException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum Code {
+
+    // 충돌 방지를 위한 Code format
+    // X1XXX: 이상은
+    // X2XXX: 정예림
+    // ex) 이상은이 닉네임 중복 에러코드를 만든다면
+    // USER_NICKNAME_DUPLICATED(11010, HttpStatus.BAD_REQUEST, "User nickname duplicated"),
+
+    OK(0, HttpStatus.OK, "Ok"),
+
+    BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
+    VALIDATION_ERROR(10001, HttpStatus.BAD_REQUEST, "Validation error"),
+    NOT_FOUND(10002, HttpStatus.NOT_FOUND, "Requested resource is not found"),
+
+    INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
+    DATA_ACCESS_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error"),
+
+    UNAUTHORIZED(40000, HttpStatus.UNAUTHORIZED, "User unauthorized");
+
+
+    private final Integer code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public String getMessage(Throwable e) {
+        return this.getMessage(this.getMessage() + " - " + e.getMessage());
+        // 결과 예시 - "Validation error - Reason why it isn't valid" 
+    }
+
+    public String getMessage(String message) {
+        return Optional.ofNullable(message)
+                .filter(Predicate.not(String::isBlank))
+                .orElse(this.getMessage());
+    }
+
+    public static Code valueOf(HttpStatus httpStatus) {
+        if (httpStatus == null) {
+            throw new GeneralException("HttpStatus is null.");
+        }
+
+        return Arrays.stream(values())
+                .filter(errorCode -> errorCode.getHttpStatus() == httpStatus)
+                .findFirst()
+                .orElseGet(() -> {
+                    if (httpStatus.is4xxClientError()) {
+                        return Code.BAD_REQUEST;
+                    } else if (httpStatus.is5xxServerError()) {
+                        return Code.INTERNAL_ERROR;
+                    } else {
+                        return Code.OK;
+                    }
+                });
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", this.name(), this.getCode());
+    }
+}
