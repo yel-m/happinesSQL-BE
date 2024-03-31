ae00e05d1a20fa80d7bb3b7a3651023e1659859c
yel-m <coin6442@gmail.com>
2024-03-31 19:16:26 +0900

    feat : 파일 업로드 & 삭제 기능 구현

---
+++ src/main/java/com/hobak/happinessql/global/infra/s3/AwsS3Service.java
@@ -0,0 +1,69 @@
+package com.hobak.happinessql.global.infra.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.hobak.happinessql.global.exception.GeneralException;
+import com.hobak.happinessql.global.response.Code;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+
+@Service
+@RequiredArgsConstructor
+public class AwsS3Service {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    public String uploadFile(MultipartFile multipartFile){
+
+        if (multipartFile == null || multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String fileName = createFileName(multipartFile.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize());
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        System.out.println(multipartFile.getContentType());
+
+        try(InputStream inputStream = multipartFile.getInputStream()){
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e){
+            throw new GeneralException(Code.INTERNAL_ERROR, "파일 업로드에 실패했습니다.");
+        }
+
+        return fileName;
+    }
+
+    // 먼저 파일 업로드시, 파일명을 난수화하기 위해 UUID 를 활용하여 난수를 돌린다.
+    public String createFileName(String fileName){
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    // file 형식이 잘못된 경우를 확인하기 위해 만들어진 로직이며, 파일 타입과 상관없이 업로드할 수 있게 하기위해, "."의 존재 유무만 판단한다.
+    private String getFileExtension(String fileName){
+        try{
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e){
+            throw new GeneralException(Code.BAD_REQUEST, "잘못된 형식의 파일" + fileName + ") 입니다.");
+        }
+    }
+
+
+    public void deleteFile(String fileName){
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
+    }
+}
