d26f85e8b34a77bac450dd7ec399416efdeabf73
yel-m <coin6442@gmail.com>
2024-04-15 03:10:58 +0900

    fix : RecordImgRepository 이름 변경 (image -> img)

--- src/main/java/com/hobak/happinessql/domain/record/repository/RecordImgRepository.java
+++ src/main/java/com/hobak/happinessql/domain/record/repository/RecordImgRepository.java
@@ -3,5 +3,5 @@ package com.hobak.happinessql.domain.record.repository;
 import com.hobak.happinessql.domain.record.domain.RecordImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecordImageRepository extends JpaRepository<RecordImg, Long> {
+public interface RecordImgRepository extends JpaRepository<RecordImg, Long> {
 }
