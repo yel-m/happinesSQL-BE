1339d7e6bb0c212ea816388da311cda791ccb914
yel-m <coin6442@gmail.com>
2024-04-27 17:12:13 +0900

    refactor : 필요없는 import문 삭제

--- src/main/java/com/hobak/happinessql/domain/record/application/RecordCreateService.java
+++ src/main/java/com/hobak/happinessql/domain/record/application/RecordCreateService.java
@@ -19,8 +19,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class RecordCreateService {
