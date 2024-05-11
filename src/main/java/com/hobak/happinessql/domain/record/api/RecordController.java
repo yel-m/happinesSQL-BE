5af91cdf8000c5a549a0c56e20d3887acfbe5043
yel-m <coin6442@gmail.com>
2024-05-11 20:41:08 +0900

    chore : userId를 쿼리 파라미터로 받도록 수정

--- src/main/java/com/hobak/happinessql/domain/record/api/RecordController.java
+++ src/main/java/com/hobak/happinessql/domain/record/api/RecordController.java
@@ -7,8 +7,6 @@ import com.hobak.happinessql.domain.record.converter.RecordConverter;
 import com.hobak.happinessql.domain.record.dto.RecordCreateRequestDto;
 import com.hobak.happinessql.domain.record.dto.RecordCreateResponseDto;
 import com.hobak.happinessql.domain.record.dto.RecordResponseDto;
-import com.hobak.happinessql.domain.user.domain.Gender;
-import com.hobak.happinessql.domain.user.domain.User;
 import com.hobak.happinessql.domain.user.repository.UserRepository;
 import com.hobak.happinessql.global.response.DataResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -36,23 +34,11 @@ public class RecordController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public DataResponseDto<Object> createRecord(
             @Valid @RequestPart(value="content") RecordCreateRequestDto requestDto,
-            @RequestPart(required = false) MultipartFile img
+            @RequestPart(required = false) MultipartFile img,
+            Long userId
         ) {
 
-        // TODO: 임시값 : 로그인 / 로그아웃 구현 시 수정
-        // 한 번 실행 시 임시 유저가 만들어지므로 1회 실행 후에는 이 부분은 주석 처리하고 실행하면 됩니다.
-        System.out.println(requestDto.getMemo());
-        User user = User.builder()
-                .username("hobak")
-                .password("happy")
-                .name("사그미")
-                .age(22)
-                .gender(Gender.FEMALE)
-                .build();
-        User newUser = userRepository.save(user);
-        System.out.println("userId : " + newUser.getUserId());
-
-        Long recordId = recordCreateService.createRecord(newUser.getUserId(), requestDto, img);
+        Long recordId = recordCreateService.createRecord(userId, requestDto, img);
         RecordCreateResponseDto recordCreationResponseDto = RecordConverter.toRecordCreateResponseDto(recordId);
 
         return DataResponseDto.of(recordCreationResponseDto, "행복 기록이 저장되었습니다.");
