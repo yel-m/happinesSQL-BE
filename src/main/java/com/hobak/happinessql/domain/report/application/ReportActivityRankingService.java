663c50fa21326383125364d6dc6a2fe850fafd58
yel-m <coin6442@gmail.com>
2024-05-29 11:05:20 +0900

    feat : 전체, 연간, 월간 모든 활동 랭킹 조회 API 구축

--- src/main/java/com/hobak/happinessql/domain/report/application/ReportActivityRankingService.java
+++ src/main/java/com/hobak/happinessql/domain/report/application/ReportActivityRankingService.java
@@ -39,4 +39,27 @@ public class ReportActivityRankingService {
         List<Record> records = recordRepository.findAllByCreatedAtBetweenAndUser(startOfMonthDateTime, endOfMonthDateTime, user);
         return ActivityHappinessAnalyzer.getActivityRankings(records, 3);
     }
+
+    public List<ActivityHappinessDto> getAllActivityRankings(User user) {
+        List<Record> records = recordRepository.findAllByUser(user);
+        return ActivityHappinessAnalyzer.getActivityRankings(records);
+    }
+
+    public List<ActivityHappinessDto> getYearlyActivityRankings(User user) {
+        int currentYear = LocalDate.now().getYear();
+        LocalDateTime startOfYear = LocalDateTime.of(currentYear, 1, 1, 0, 0);
+        LocalDateTime endOfYear = LocalDateTime.of(currentYear, 12, 31, 23, 59, 59);
+        List<Record> records = recordRepository.findAllByCreatedAtBetweenAndUser(startOfYear, endOfYear, user);
+        return ActivityHappinessAnalyzer.getActivityRankings(records);
+    }
+
+    public List<ActivityHappinessDto> getMonthlyActivityRankings(User user) {
+        LocalDate today = LocalDate.now();
+        LocalDate startOfMonth = LocalDate.now().withDayOfMonth(1);
+        LocalDate endOfMonth = LocalDate.now().withDayOfMonth(today.lengthOfMonth());
+        LocalDateTime startOfMonthDateTime = startOfMonth.atStartOfDay();
+        LocalDateTime endOfMonthDateTime = endOfMonth.atTime(23, 59, 59);
+        List<Record> records = recordRepository.findAllByCreatedAtBetweenAndUser(startOfMonthDateTime, endOfMonthDateTime, user);
+        return ActivityHappinessAnalyzer.getActivityRankings(records);
+    }
 }
